f6cde34ffa89684f600ca018d00ddeb6c60c7c6c
HZjiadong <guojiadonghz@gmail.com>
2021-02-23 19:27:13 +0800

    Update stocks.m
    add commentaires

--- stocks.m
+++ stocks.m
@@ -14,16 +14,16 @@ MP_contrainte = [1 2 3 1 1 2;
                  2 2 1 2 2 1;
                  1 0 3 2 2 1];
              
-%Table5: co?t horaire des machines par heure
+%Table5: cost horaire des machines par heure
 cout_machine = [2 2 3 3 2 3 3]';
 
-%Table5': co?t horaire des machine par minutes
+%Table5': cost horaire des machine par minutes
 cout_produit_temps = Machine_contrainte' * cout_machine / 60;
 
-%Table4: co?t d'achat des matières premières
+%Table4: cost d'achat des matières premières
 cout_matiere = [3 2 1];
 
-%Table4':co?t des matières premières pour chaque produire    
+%Table4':cost des matières premières pour chaque produire    
 cout_produit_mp = MP_contrainte' * cout_matiere';
 
 %Table4:prix de vente des produits finis    
@@ -34,6 +34,7 @@ revenu_brut = [55 37 60 45 35 30]';
 benefice_contraint = revenu_brut - cout_produit_mp - cout_produit_temps;
 
 A = [A1; -benefice_contraint'];
+%8000是判断工厂正常工作状态的最小值，即“产值超过8000就视为正常状态”
 b = [b1; -8000];
 %这里运用的是Table2对于每个产品需要的MP的体积+产品本身的体积；
 %为了简化问题，MP的体积=产品的体积
