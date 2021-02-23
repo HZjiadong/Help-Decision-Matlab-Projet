bd54480d944419e6d6208eda7385520f961cabb4
HZjiadong <guojiadonghz@gmail.com>
2021-02-23 19:26:52 +0800

    Update concordanceEqPoid.m
    add commentaires

--- concordanceEqPoid.m
+++ concordanceEqPoid.m
@@ -1,4 +1,12 @@
+%Dans un premier temps, on consid¨¨re que tous les contrainte one m¨ºme poid, c-a-d P(g1)=P(g2)=P(g3)=P(g4)=1
 function [concord] = concordanceEqPoid(mat)
+%mat:matrice des judgements
+%dim(mat)=a*b, avec a=nb de lignes & b=nb de colonnes
+%size(mat,1)=a:nombre de Solutions
+%size(mat,2)=b:nombre de Critere
+%size(mat,3)=1--->
+%because there's no third dimension, so size returns 1 by convention
+
 nbSol = size(mat, 1);
 nbCrit = size(mat, 2);
 concord = zeros(nbSol, nbSol);
