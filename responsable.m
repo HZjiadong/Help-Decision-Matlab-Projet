d258502c65881e9ef58eedfd144d1917a50a3096
HZjiadong <guojiadonghz@gmail.com>
2021-02-23 19:27:09 +0800

    Update responsable.m
    add commentaires

--- responsable.m
+++ responsable.m
@@ -1,8 +1,12 @@
 [A, b, lb] = AD_initialisation();
-%responsable comptable?Atelier?
+%multicritère的所有代码都要放进来
+
+
 % pour maximiser tous les produit, matrice de coifficient des six produits sont tous egaux aux 1
 %pour utiliser linprog, il faut passer Max au Min 
 %donc il faut traduire la matrice f vers un minimum avec tous egaux aux -1
+
+%responsable Atelier，追求产量最大化，也就是每个产品的系数都是最大的
 f = [1 1 1 1 1 1]';
 
 res = linprog(-f,A,b,[],[],lb,[]);
